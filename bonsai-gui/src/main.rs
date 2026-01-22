07d33ab4ecd0599cfb5ee4778d9cd57c3c8b71e6
niblit <git@niblit.dev>
2026-01-22 15:44:39 -0600

    Update GUI to show the logo (as a placeholder)

--- bonsai-gui/src/main.rs
+++ bonsai-gui/src/main.rs
@@ -8,18 +8,13 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
-
     view! {
-        <button class="text-9xl text-blue-900"
-            on:click=move |_| {*set_count.write() += 1;}
-        >
-            "Click me: "
-            {count}
-        </button>
-        <p class="text-2xl text-blue-900">
-            "Double count: "
-            {move || count.get() * 2}
-        </p>
+        <div class="min-h-screen flex items-center justify-center bg-zinc-900">
+            <img
+                src="/static/bonsai.svg"
+                alt="Bonsai Chess Logo"
+                class="w-1/4 h-1/4"
+            />
+        </div>
     }
 }
