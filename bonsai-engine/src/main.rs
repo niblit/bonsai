8bb207c9580e57c9d8b41533cf9859668bb51fed
niblit <git@niblit.dev>
2026-02-16 08:22:18 -0600

    Update fen position to a mate in 5 plies

--- bonsai-engine/src/main.rs
+++ bonsai-engine/src/main.rs
@@ -2,7 +2,10 @@ use bonsai_chess::prelude::BoardFrontend;
 use bonsai_engine::best_move;
 
 fn main() {
-    let board = BoardFrontend::from_fen("k2q4/pp5p/1b3p2/8/3N1Q2/P1P1pPP1/1P1r3P/1K2R3 b - - 2 29");
+    // from lichess.org mate in 3 puzzles
+    // solution is 26... Nf3+ 27. Kh1 Rg1+ 28. Rxg1 Rxg1#
+    let fen = "2k3r1/ppp1n1r1/3p1p1b/1P2p1nQ/2PPP2P/P1N5/5P2/R1B2RK1 b - - 0 26";
+    let board = BoardFrontend::from_fen(fen);
 
     let engine_move = best_move(board, 6);
 
